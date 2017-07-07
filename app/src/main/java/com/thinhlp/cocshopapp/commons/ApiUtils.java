52cb1db923563e42c09b9a56556062b7f2399dd7
ThinhLP <thinhlp208@gmail.com>
2017-07-07 16:21:07 +0700

    [Issue #2] Add button Add to cart [ThinhLP]

--- app/src/main/java/com/thinhlp/cocshopapp/commons/ApiUtils.java
+++ app/src/main/java/com/thinhlp/cocshopapp/commons/ApiUtils.java
@@ -10,8 +10,8 @@ import com.thinhlp.cocshopapp.services.UserService;
 
 public class ApiUtils {
 
-   // public static final String BASE_URL = "http://192.168.2.31:8080/api/";
-    public static final String BASE_URL = "http://192.168.43.115:8080/api/";
+    public static final String BASE_URL = "http://192.168.2.31:8080/api/";
+    //public static final String BASE_URL = "http://192.168.43.115:8080/api/";
 
     public static UserService getUserService() {
         return RetrofitClient.getClient(BASE_URL).create(UserService.class);
